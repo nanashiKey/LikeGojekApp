4a111d29a5af0aaf0074fe7d370011c53ff344ee
Irfan <assidiq.irfan@gmail.com>
2020-10-01 17:31:09 +0700

    Initial commit

---
+++ app/src/main/java/com/irfandev/project/likegojekapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.irfandev.project.likegojekapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
